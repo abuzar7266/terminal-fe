8d37b1acc4fa45afdd035edff334dd2940324001
Abuzar <63154720+abuzar7266@users.noreply.github.com>
2023-11-04 13:28:55 +0500

    upload csv command (#4)
    * command for fetch price from api added (#1) (#2)
    
    Co-authored-by: abuzar2019 <abuzar.maher@mergestack.com>
    
    * Create main.yml
    
    * workflow removed
    
    * select csv file method added
    
    * upload csv draw file complete
    
    ---------
    
    Co-authored-by: abuzar2019 <abuzar.maher@mergestack.com>

--- src/config/helpers.ts
+++ src/config/helpers.ts
@@ -9,6 +9,60 @@ export const console_about = (args: any, print: any) => {
   );
 };
 
+async function selectAndUpload(print: any): Promise<File | null> {
+  return new Promise<File | null>((resolve) => {
+    const inputElement = document.createElement("input");
+    inputElement.type = "file";
+    inputElement.style.display = "none";
+    inputElement.accept = ".csv";
+
+    const handleChange = async (event: Event) => {
+      const files = (event.target as HTMLInputElement).files;
+      if (files && files.length > 0) {
+        const formData = new FormData(); // Create a FormData object
+
+        if (files[0]) {
+            formData.append('file', files[0]); // Append the file to the FormData object
+        }
+        try{
+          const response = await fetch('http://127.0.0.1:8080/cli/upload', { method: 'POST', body: formData});
+          if(response.status===200){
+            let {msg} = await response.json();
+            print(msg);
+          }
+          else
+            print(`Error: failed to upload the selected file`);
+          resolve(files[0]);
+        }catch(err){
+          print(`Error: failed to connect with server`);
+        }
+      } else {
+        resolve(null);
+      }
+      document.body.removeChild(inputElement);
+    };
+
+    inputElement.addEventListener("change", handleChange);
+
+    document.body.appendChild(inputElement);
+    inputElement.click();
+
+    const checkForFileSelection = () => {
+      if (inputElement.parentNode) {
+        setTimeout(checkForFileSelection, 100);
+      } else {
+        inputElement.removeEventListener("change", handleChange);
+        resolve(null);
+      }
+    };
+
+    checkForFileSelection();
+  });
+}
+
+export const command_upload = (args: any, print: any) => {
+  selectAndUpload(print);
+};
 export const fetchPrice = async (symbol: string) => {
   const response = await fetch(
     `https://api.binance.com/api/v3/avgPrice?symbol=${symbol}`
@@ -63,4 +117,5 @@ export const TERMINAL_COMMANDS = {
     },
   },
   "fetch-price": { method: console_fetchPrice },
+  upload: { method: command_upload },
 };
